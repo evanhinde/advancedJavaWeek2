b16e5cc7391c988abab65de1b3b687439af67161
evanhinde <200529232@student.georgianc.on.ca>
2023-09-12 22:52:37 -0400

    code commited

--- src/main/java/com/example/advancedjavaweek2/Book.java
+++ src/main/java/com/example/advancedjavaweek2/Book.java
@@ -1,2 +1,108 @@
-package com.example.advancedjavaweek2;public class Book {
+package com.example.advancedjavaweek2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    //variables
+    private int bookId;
+    private String bookName, author, genre;
+    private double price;
+    private boolean isAvailable;
+
+    //constructors
+    public Book(int bookId, String bookName, String author, String genre, double price, boolean isAvailable) {
+        setBookId(bookId);
+        setBookName(bookName);
+        setAuthor(author);
+        setGenre(genre);
+        setPrice(price);
+        setAvailable(isAvailable);
+    }
+
+    //getters and setters
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        if(bookId <= 0) {
+            throw new IllegalArgumentException("Book ID must be greater than 0");
+        }
+        else {
+            this.bookId = bookId;
+        }
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        if(bookName.isBlank()){
+        throw new IllegalArgumentException("Book name cannot be empty");
+        }
+        else {
+        this.bookName = bookName;
+        }
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        if(author.isBlank()){
+        throw new IllegalArgumentException("Author cannot be empty");
+        }
+        else {
+        this.author = author;
+        }
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        List<String> validGenres = findGenres();
+        if(validGenres.contains(genre)) {
+        this.genre = genre;
+        }
+        else {
+            throw new IllegalArgumentException("Genre should be from the list: " + validGenres);
+        }
+    }
+
+    public static List<String> findGenres() {
+        return Arrays.asList("Comedy", "Thriller", "Fiction", "Non-Fiction", "Fantasy", "Crime", "History");
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if(price < 0) {
+        throw new IllegalArgumentException("Price cannot be less than 0");
+        }
+        else {
+        this.price = price;
+        }
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+//toString
+
+    @Override
+    public String toString() {
+        return bookId + ": The price of " + bookName + " written by " + author + " of genre " + genre + " is " + price + ".Availability: " + (isAvailable ? "Yes" : "No");
+    }
 }
--- src/main/java/com/example/advancedjavaweek2/CreateBookController.java
+++ src/main/java/com/example/advancedjavaweek2/CreateBookController.java
@@ -1,2 +1,69 @@
-package com.example.advancedjavaweek2;public class CreateBookController {
+package com.example.advancedjavaweek2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateBookController implements Initializable {
+
+    @FXML
+    private TextField authorTextField;
+
+    @FXML
+    private CheckBox availabilityCheckBox;
+
+    @FXML
+    private TextField bookNameTextField;
+
+    @FXML
+    private Label finalLabel;
+
+    @FXML
+    private ComboBox<String> genreComboBox;
+
+    @FXML
+    private TextField priceTextField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("I am in initialize method.");
+        // make label invisible
+        finalLabel.setVisible(false);
+
+        // populate genres into combobox
+        genreComboBox.getItems().addAll(Book.findGenres());
+    }
+
+    @FXML
+    void saveBook(ActionEvent event) {
+        System.out.println("Button Clicked");
+
+        try {
+            // store user data in variables
+            String bookName = bookNameTextField.getText();
+            String author = authorTextField.getText();
+            String genre = genreComboBox.getSelectionModel().getSelectedItem();
+            double price = (!priceTextField.getText().isBlank()) ? Double.parseDouble(priceTextField.getText()) : -1;
+            boolean isAvailable = availabilityCheckBox.isSelected();
+
+            // create book object
+            Book book = new Book(1, bookName, author, genre, price, isAvailable);
+
+            // show the values to the user
+            finalLabel.setVisible(true);
+            finalLabel.setText(book.toString());
+        } catch (Exception e) {
+            // show proper exception message to user
+            finalLabel.setVisible(true);
+            finalLabel.setText(e.getMessage());
+        }
+    }
 }
+
